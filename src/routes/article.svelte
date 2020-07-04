1656c064e1c60d121ec4ecad44ef4b3560885bd9
Beto Sosa <betososah@gmail.com>
2020-07-03 22:09:03 -0500

    build

---
+++ src/routes/article.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`http://my-json-server.typicode.com/betososa/sapper-test/articles/1`).then(r => r.json()).then(article => {
+      return { article };
+    });
+  }
+</script>
+
+<script>
+  export let article;
+</script>
+
+<style>
+  ul {
+    margin: 0 0 1em 0;
+    line-height: 1.5;
+  }
+</style>
+
+<svelte:head>
+  <title>{article.title}</title>
+  <meta property="og:title" content={article.title} />
+  <meta property="og:image" content={article.image} />
+  <meta property="og:description" content={article.description} />
+</svelte:head>
+
+<h1>{article.title}</h1>
+
+<img src={article.image}>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,13 +1,13 @@
 <script context="module">
 	export function preload({ params, query }) {
-		return this.fetch(`https://jsonplaceholder.typicode.com/todos/1`).then(r => r.json()).then(todo => {
-			return { todo };
+		return this.fetch(`http://my-json-server.typicode.com/betososa/sapper-test/articles/1`).then(r => r.json()).then(article => {
+			return { article };
 		});
 	}
 </script>
 
 <script>
-	export let todo;
+	export let article;
 </script>
 
 <style>
@@ -18,21 +18,12 @@
 </style>
 
 <svelte:head>
-	<title>{todo.title}</title>
-	<meta property="og:title" content={todo.title} />
-	<meta property="og:type" content="video.movie" />
-	<meta property="og:url" content={todo.title} />
-	<meta property="og:image" content="http://ia.media-imdb.com/images/rock.jpg" />
+	<title>{article.title}</title>
+	<meta property="og:title" content={article.title} />
+	<meta property="og:image" content={article.image} />
+	<meta property="og:description" content={article.description} />
 </svelte:head>
 
-<h1>Recent posts</h1>
+<h1>{article.title}</h1>
 
-<ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
-	{/each}
-</ul>
+<img src={article.image}>
